9d9c16bba17b8c1c191eeb03c8469029bce3b919
Bibhuti Bhusan Dash <bibhutibhushandash0@gmail.com>
2026-02-19 19:35:12 +0530

    Updated project files

--- authproject_15_10/settings.py
+++ authproject_15_10/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'authproject_15_10.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlineexamportal',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
